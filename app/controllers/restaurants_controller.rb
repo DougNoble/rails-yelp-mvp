2efe3252fb41897bb09fa42192ad4201c8387c40
Doug Noble <douglasjmnoble@googlemail.com>
2019-11-13 16:17:26 +0000

    rails-yelp-mvp

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,34 @@
 class RestaurantsController < ApplicationController
+
+  # GET /restaurants
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  # GET /restaurants/1
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    @review = Review.new
+  end
+
+  # GET /restaurants/new
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  # POST /restaurants
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:  'belgian',
+    phone_number:   '07904 *** ***',
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:  'italian',
+    phone_number:  '07904 *** ***',
+  },
+  {
+    name:         'City Caphe',
+    address:      '17 Ironmonger Lane, London EC2V 8EY',
+    category:  'chinese',
+    phone_number:   '07904 *** ***',
+  },
+    {
+    name:         'Circolo Populare',
+    address:      '40-41 Rathbone Pl, Fitzrovia, London W1T 1HX',
+    category:  'italian',
+    phone_number:   '07904 *** ***',
+  },
+    {
+    name:         'Eat Tokyo',
+    address:      '17 Notting Hill Gate, London W11 3JQ',
+    category:  'japanese',
+    phone_number:   '07904 *** ***',
+  },
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
